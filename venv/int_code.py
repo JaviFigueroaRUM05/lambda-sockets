7d1bcd1e6afa0d40f64eb3e2f03faec8a49626f2
Favier Figueroa <javier.figueroa15@upr.edu>
2019-12-10 14:03:27 -0400

    fixed int_code.py

--- venv/int_code.py
+++ venv/int_code.py
@@ -1,34 +1,38 @@
-import sockets
+import socket
 
 class Lambda_Sockets:
 
-    host_ip = '127.0.0.1'
-    host_port = 12345
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self):
+        self.host_ip = '127.0.0.1'
+        self.host_port = 12345
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn = 0
+        self.addr = 0
 
-    @staticmethod
-    def init():
-        s.bind((host_ip, port_ip))
+    def init(self):
+        self.s.bind((self.host_ip, self.host_port))
 
-    @staticmethod
-    def listen():
-        s.listen()
-        conn, addr = s.accept()
+    def listen(self):
+        self.s.listen()
+        self.conn, self.addr = self.s.accept()
 
-    @staticmethod
-    def connect(host, port):
-        s.connect((host, port))
+    def connect(self, host, port):
+        self.s.connect((host, port))
 
-    @staticmethod
-    def disconnect():
-        s.close()
+    def disconnect(self):
+        self.s.close()
 
-    @staticmethod
-    def send(data):
-        s.sendall(data.encode())
+    def client_send(self, data):
+        self.s.sendall(data.encode())
 
-    @staticmethod
-    def receive():
-        message = conn.recv(1024)
+    def server_send(self, data):
+        self.conn.sendall(data.encode())
+
+    def server_receive(self):
+        message = self.conn.recv(1024)
+        print(message.decode())
+    
+    def client_receive(self):
+        message = self.s.recv(1024)
         print(message.decode())
 
